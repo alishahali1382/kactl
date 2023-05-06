244fa281a4c9faf586051f0e0f3de330dbb4b192
alishahali1382 <alishahali1382@gmail.com>
2023-05-06 14:59:04 +0330

    first commit

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
 typedef long long ll;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
+
+#define all(x) begin(x), end(x)
+#define SZ(x) ((int)(x).size())
+#define debug(x) cerr<<#x<<"="<<x<<endl;
+#define kill(x) return cout<<x<<"\n", 0;
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+
+const int MOD=1e9+7;
+const int inf=1e9+10;
+const ll INF=1e18+1e4;
+const int MAXN=1e6+10;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+
+	return 0;
 }
