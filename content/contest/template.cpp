c18ddf595c6c0c5737d385d17c3f6a5143f8d927
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-05-08 14:57:00 +0330

    add X Y macro to template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -8,14 +8,16 @@ typedef vector<int> vi;
 
 #define all(x) begin(x), end(x)
 #define SZ(x) ((int)(x).size())
-#define debug(x) cerr<<#x<<"="<<x<<endl;
-#define kill(x) return cout<<x<<"\n", 0;
+#define debug(x) cerr << #x << "=" << x << endl;
+#define kill(x) return cout << x << "\n", 0;
 #define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define X first
+#define Y second
 
-const int MOD=1e9+7;
-const int inf=1e9+10;
-const ll INF=1e18+1e4;
-const int MAXN=1e6+10;
+const int MOD = 1e9+7;
+const int inf = 1e9+10;
+const ll INF = 1e18+1e4;
+const int MAXN = 1e6+10;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
