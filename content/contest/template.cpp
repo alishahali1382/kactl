282ca22e7e46aab6a185320f2a308840fcfdba2b
alishahali1382 <alishahali1382@gmail.com>
2023-05-16 16:16:26 +0330

    fix latex bug

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
 
 #define all(x) begin(x), end(x)
 #define SZ(x) ((int)(x).size())
-#define debug(x) cerr << #x << "=" << x << endl;
 #define kill(x) return cout << x << "\n", 0;
 #define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define X first
