72390df2246fb7b3b9f6f61328f705e901905248
alishahali1382 <alishahali1382@gmail.com>
2023-05-18 00:17:56 +0330

    add my fft and ntt

--- content/numerical/NumberTheoreticTransform.h
+++ content/numerical/NumberTheoreticTransform.h
@@ -1,53 +1,35 @@
 /**
- * Author: chilli
- * Date: 2019-04-16
+ * Author: Ali Shahali
+ * Date: 2020-06-16
  * License: CC0
- * Source: based on KACTL's FFT
- * Description: ntt(a) computes $\hat f(k) = \sum_x a[x] g^{xk}$ for all $k$, where $g=\text{root}^{(mod-1)/N}$.
- * N must be a power of 2.
- * Useful for convolution modulo specific nice primes of the form $2^a b+1$,
- * where the convolution result has size at most $2^a$. For arbitrary modulo, see FFTMod.
-   \texttt{conv(a, b) = c}, where $c[x] = \sum a[i]b[x-i]$.
-   For manual convolution: NTT the inputs, multiply
-   pointwise, divide by n, reverse(start+1, end), NTT back.
- * Inputs must be in [0, mod).
+ * Source: https://codeforces.com/contest/438/submission/83926262
+ * Description: same as fft, be careful for mod. 998244353 is ok.
  * Time: O(N \log N)
- * Status: stress-tested
+ * Status: accepted-on-codeforces
  */
 #pragma once
 
-#include "../number-theory/ModPow.h"
-
-const ll mod = (119 << 23) + 1, root = 62; // = 998244353
-// For p < 2^30 there is also e.g. 5 << 25, 7 << 26, 479 << 21
-// and 483 << 21 (same root). The last two are > 10^9.
-typedef vector<ll> vl;
-void ntt(vl &a) {
-	int n = SZ(a), L = 31 - __builtin_clz(n);
-	static vl rt(2, 1);
-	for (static int k = 2, s = 2; k < n; k *= 2, s++) {
-		rt.resize(n);
-		ll z[] = {1, modpow(root, mod >> s)};
-		rep(i,k,2*k) rt[i] = rt[i / 2] * z[i & 1] % mod;
+typedef vector<ll> Poly;
+void NTT(Poly &A, bool inv){
+	int n=A.size(), lg=0;
+	while ((1<<lg)<n) lg++; A.resize(n=(1<<lg), 0);
+	for (int i=1; i<n; i++){
+		rev[i]=(rev[i>>1]>>1) | ((i&1)<<(lg-1));
+		if (rev[i]<i) swap(A[rev[i]], A[i]);
 	}
-	vi rev(n);
-	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
-	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
-			ll z = rt[j + k] * a[i + j + k] % mod, &ai = a[i + j];
-			a[i + j + k] = ai - z + (z > ai ? mod : 0);
-			ai += (ai + z >= mod ? z - mod : z);
+	for (int len=1; len<n; len<<=1){
+		ll wn=powmod(3, mod/2/len);
+		if (inv) wn=powmod(wn, mod-2);
+		for (int i=0; i<n; i+=2*len){
+			ll w=1;
+			for (int j=i; j<i+len; j++){
+				ll x=A[j], y=A[j+len]*w;
+				A[j]=(x+y)%mod;
+				A[j+len]=(x-y)%mod;
+				w=w*wn%mod;
+			}
 		}
-}
-vl conv(const vl &a, const vl &b) {
-	if (a.empty() || b.empty()) return {};
-	int s = SZ(a) + SZ(b) - 1, B = 32 - __builtin_clz(s), n = 1 << B;
-	int inv = modpow(n, mod - 2);
-	vl L(a), R(b), out(n);
-	L.resize(n), R.resize(n);
-	ntt(L), ntt(R);
-	rep(i,0,n) out[-i & (n - 1)] = (ll)L[i] * R[i] % mod * inv % mod;
-	ntt(out);
-	return {out.begin(), out.begin() + s};
+	}
+	ll invn=powmod(n, mod-2);
+	if (inv) for (int i=0; i<n; i++) A[i]=A[i]*invn%mod;
 }
