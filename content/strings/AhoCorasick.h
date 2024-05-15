6b65cf42a46585c308222e52cbfa2b9d48e53140
Ali Shahali <ali.shahali@divar.ir>
2024-05-15 15:31:33 +0330

    add better Aho

--- content/strings/AhoCorasick.h
+++ content/strings/AhoCorasick.h
@@ -17,69 +17,32 @@
  */
 #pragma once
 
-struct AhoCorasick {
-	enum {alpha = 26, first = 'A'}; // change this!
-	struct Node {
-		// (nmatches is optional)
-		int back, next[alpha], start = -1, end = -1, nmatches = 0;
-		Node(int v) { memset(next, v, sizeof(next)); }
-	};
-	vector<Node> N;
-	vi backp;
-	void insert(string& s, int j) {
-		assert(!s.empty());
-		int n = 0;
-		for (char c : s) {
-			int& m = N[n].next[c - first];
-			if (m == -1) { n = m = SZ(N); N.emplace_back(-1); }
-			else n = m;
+const int MAXN=1e5, SGM=26;
+struct Aho{
+	int C[MAXN][SGM], F[MAXN], ts;
+	int Q[MAXN], QL, QR;
+	int AddTrie(string &s){
+		int v=0;
+		for (char ch:s){
+			if (!C[v][ch-'a']) C[v][ch-'a']=++ts;
+			v=C[v][ch-'a'];
 		}
-		if (N[n].end == -1) N[n].start = j;
-		backp.push_back(N[n].end);
-		N[n].end = j;
-		N[n].nmatches++;
+		return v;
 	}
-	AhoCorasick(vector<string>& pat) : N(1, -1) {
-		rep(i,0,SZ(pat)) insert(pat[i], i);
-		N[0].back = SZ(N);
-		N.emplace_back(0);
-
-		queue<int> q;
-		for (q.push(0); !q.empty(); q.pop()) {
-			int n = q.front(), prev = N[n].back;
-			rep(i,0,alpha) {
-				int &ed = N[n].next[i], y = N[prev].next[i];
-				if (ed == -1) ed = y;
-				else {
-					N[ed].back = y;
-					(N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
-						= N[y].end;
-					N[ed].nmatches += N[y].nmatches;
-					q.push(ed);
+	void BuildAho(){
+		for (int i=0; i<SGM; i++) if (C[0][i]) Q[QR++]=C[0][i];
+		while (QL<QR){
+			int v=Q[QL++];
+			// if (ID[v]) UP[v]=v;
+			// else UP[v]=UP[F[v]];
+			// G[F[v]].pb(v);
+			for (int i=0; i<SGM; i++){
+				if (!C[v][i]) C[v][i]=C[F[v]][i];
+				else{
+					Q[QR++]=C[v][i];
+					F[C[v][i]]=C[F[v]][i];
 				}
 			}
 		}
-	}
-	vi find(string word) {
-		int n = 0;
-		vi res; // ll count = 0;
-		for (char c : word) {
-			n = N[n].next[c - first];
-			res.push_back(N[n].end);
-			// count += N[n].nmatches;
-		}
-		return res;
-	}
-	vector<vi> findAll(vector<string>& pat, string word) {
-		vi r = find(word);
-		vector<vi> res(SZ(word));
-		rep(i,0,SZ(word)) {
-			int ind = r[i];
-			while (ind != -1) {
-				res[i - SZ(pat[ind]) + 1].push_back(ind);
-				ind = backp[ind];
-			}
-		}
-		return res;
 	}
 };
